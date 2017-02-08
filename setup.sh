a3c7665761b2d0e8b95bd07adfcad43a757ee49a
Claude LE BRIS <lebris@domisys.com>
2017-02-08 13:55:18 +0100

    Update dotfiles

--- setup.sh
+++ setup.sh
@@ -2,14 +2,22 @@
 
 dotfilesDirectory=~/.dotfiles
 
-RESTORE='\033[0m'
-yellow='\033[00;33m'
+RESTORE='\e[0m'
+yellow='\e[00;33m'
 
-files=`ls -1 $dotfilesDirectory/symlinks`
+files=`ls -1 $dotfilesDirectory/dotfiles`
 for file in $files; do
-    echo $yellow"Creating symlink for $file"$RESTORE
-    ln -sf $dotfilesDirectory/symlinks/$file ~/.$file
+    echo -e $yellow"Creating symlink for $file"$RESTORE
+    ln -sf $dotfilesDirectory/dotfiles/$file ~/.$file
 done
 
-echo "\nNow you have to run 'source ~/.bashrc'"
+echo
+if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
+    echo "Installing vim's plugin manager Vundle"
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
 
+echo -e $yellow"Installing vim plugins"$RESTORE
+vim +PluginInstall +qall
+
+echo -e "\nNow you have to run 'source ~/.bashrc'"
