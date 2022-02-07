6e486c3f6fdcc8529a04f82102956ea49e58bf6f
Claude LE BRIS <claude.lebris@gmail.com>
2022-02-07 16:19:17 +0100

    feat: update 2022

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-```
+Prerequisites
+ - Ubuntu : enable universe repository (for powerline)
+
+```console
+apt-get update && apt-get install -y git make powerline fonts-powerline powerline-gitstatus
+
 $ git clone git@github.com:lebris/dotfiles.git ~/.dotfiles
 $ cd ~/.dotfiles
 $ ./setup.sh
--- setup.sh
+++ setup.sh
@@ -17,11 +17,11 @@ for file in $files; do
     fi
 done
 
-echo
-if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
-    echo "Installing vim's plugin manager Vundle"
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-fi
+#echo
+#if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
+#    echo "Installing vim's plugin manager Vundle"
+#    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#fi
 
 if [ -d $dotfilesDirectory/config ]; then
     files=`ls -1 $dotfilesDirectory/config`
@@ -37,36 +37,5 @@ if [ -d $dotfilesDirectory/config ]; then
         fi
     done
 fi
-
-echo -e $yellow"Installing vim plugins"$RESTORE
-vim +PluginInstall +qall
-
-if [ -d $dotfilesDirectory/atom ]; then
-    echo -e $yellow"Installing atom snippets"$RESTORE
-    cp $dotfilesDirectory/atom/snippets.cson ~/.atom/snippets.cson # symlink with cygwin not supported :(
-fi
-
-
-echo -e $yellow"Installing bin utils"$RESTORE
-if [ ! -d "~/.bin" ]; then
-    mkdir -p ~/.bin
-fi
-for file in $(ls -1 $dotfilesDirectory/bin); do
-    SOURCE=$dotfilesDirectory/bin/$file
-    DESTINATION=~/.bin/$file
-    if [ ! -L ${DESTINATION} ]; then
-        echo -e $yellow"Creating symlink for $DESTINATION"$RESTORE
-        ln -s ${SOURCE} ${DESTINATION}
-    else
-        echo -e $yellow"Creating symlink for $DESTINATION : already existing, skipping."$RESTORE
-    fi
-done
-
-echo -e $yellow"Building docker images"$RESTORE
-for DIR in $(ls --directory images/*); do
-    cd ${DIR}
-    make build
-    cd -
-done
 
 echo -e "\nNow you have to run 'source ~/.bashrc'"
